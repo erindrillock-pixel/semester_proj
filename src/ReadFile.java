6da0c1a2b0bea29d6d91793200317f72743e2f35
erindrillock-pixel <erin.drillock@student.fairfield.edu>
2025-10-01 11:09:02 -0400

    File reader1

---
+++ src/ReadFile.java
@@ -0,0 +1,20 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("/Users/ErinDrillock/Programming/Project files/Article 1_ train.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
