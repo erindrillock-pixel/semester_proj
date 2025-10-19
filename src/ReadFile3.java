9eea173f618552cfd02c10a2cc12af75fc7a4632
erindrillock-pixel <erin.drillock@student.fairfield.edu>
2025-10-19 17:34:08 -0400

    main will print out all 3 topics and finished readfile

--- src/ReadFile3.java
+++ src/ReadFile3.java
@@ -2,7 +2,7 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-public class ReadFiles {
+public class ReadFiles3 {
     public static void main(String[] args) {
         try {
             File myObj = new File("C:/Users/laniy/Downloads/Fall 2025 Classes/CPSC 2231 Lab/Text Files/rats.txt");
