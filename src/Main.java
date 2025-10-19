b08599853bc01363f3b663b62eaea3a75a7cb54d
erindrillock-pixel <erin.drillock@student.fairfield.edu>
2025-10-19 17:32:31 -0400

    main will print out all 3 topics and finished readfile

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Topic 1: \n");
+        ReadFile.main(args);
+
+        System.out.println("\nTopic 2: \n");
+        ReadFile2.main(args);
+
+        System.out.println("\nTopic 3: \n");
+        ReadFile3.main(args);
+    }
 }
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,14 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 
 public class ReadFile {
-    static class FileStats {
-        int totalWords;
-        int uniqueWords;
-        String topWord;
-        int topCount;
-    }
 
     public static void main(String[] args) {
         String stopWordsPath = "/Users/ErinDrillock/Programming/Project files/stopwords.txt";
@@ -17,7 +15,7 @@ public class ReadFile {
         String articlePath3 = "/Users/ErinDrillock/Programming/Project files/Article 3_ roasted meat.txt";
 
         // Load stop words
-        List<String> stopWords = loadStopWords(stopWordsPath);
+        Set<String> stopWords = loadStopWords(stopWordsPath);
 
         // Process each article
         processArticle("Article 1", articlePath1, stopWords);
@@ -25,20 +23,52 @@ public class ReadFile {
         processArticle("Article 3", articlePath3, stopWords);
     }
 
-    public static void processArticle(String label, String filePath, List<String> stopWords) {
-        List<String> words = loadWordsFromFile(filePath);
-        removeStopWords(words, stopWords);
-        FileStats stats = analyzeWords(words);
+    public static void processArticle(String label, String filePath, Set<String> stopWords) {
+        ArrayList<String> words = loadWordsFromFile(filePath);
+        int totalWords = words.size();
 
-        // Print statistics
+        removeStopWords(words, stopWords);
+
+        ArrayList<String> uniqueWords = new ArrayList<>();
+        ArrayList<Integer> wordCounts = new ArrayList<>();
+
+        for (String word : words) {
+            int index = uniqueWords.indexOf(word);
+            if (index != -1) {
+                wordCounts.set(index, wordCounts.get(index) + 1);
+            } else {
+                uniqueWords.add(word);
+                wordCounts.add(1);
+            }
+        }
+
+        for (int i = 0; i < wordCounts.size() - 1; i++) {
+            for (int j = 0; j < wordCounts.size() - i - 1; j++) {
+                if (wordCounts.get(j) < wordCounts.get(j + 1)) {
+                    // Swap counts
+                    int tempCount = wordCounts.get(j);
+                    wordCounts.set(j, wordCounts.get(j + 1));
+                    wordCounts.set(j + 1, tempCount);
+
+                    // Swap corresponding words
+                    String tempWord = uniqueWords.get(j);
+                    uniqueWords.set(j, uniqueWords.get(j + 1));
+                    uniqueWords.set(j + 1, tempWord);
+                }
+            }
+        }
+
         System.out.println("\n--- " + label + " ---");
-        System.out.println("Total words: " + stats.totalWords);
-        System.out.println("Unique words: " + stats.uniqueWords);
-        System.out.println("Most frequent word: " + stats.topWord + " (" + stats.topCount + " times)");
+        System.out.println("Total words in article: " + totalWords);
+        System.out.println("Unique words: " + uniqueWords.size());
+        System.out.println("\nWords ranked by frequency:\n");
+
+        for (int i = 0; i < uniqueWords.size(); i++) {
+            System.out.println(uniqueWords.get(i) + ": " + wordCounts.get(i));
+        }
     }
 
-    // Removes stop words using an iterator
-    public static void removeStopWords(List<String> words, List<String> stopWords) {
+    public static void removeStopWords(ArrayList<String> words, Set<String> stopWords) {
         Iterator<String> iterator = words.iterator();
         while (iterator.hasNext()) {
             String word = iterator.next();
@@ -48,9 +78,8 @@ public class ReadFile {
         }
     }
 
-    // Reads stop words from file
-    public static List<String> loadStopWords(String filePath) {
-        List<String> stopWords = new ArrayList<>();
+    public static Set<String> loadStopWords(String filePath) {
+        Set<String> stopWords = new HashSet<>();
         try (Scanner reader = new Scanner(new File(filePath))) {
             while (reader.hasNext()) {
                 stopWords.add(reader.next().toLowerCase());
@@ -61,12 +90,12 @@ public class ReadFile {
         return stopWords;
     }
 
-    // Reads words from an article file
-    public static List<String> loadWordsFromFile(String filePath) {
-        List<String> words = new ArrayList<>();
+    public static ArrayList<String> loadWordsFromFile(String filePath) {
+        ArrayList<String> words = new ArrayList<>();
         try (Scanner reader = new Scanner(new File(filePath))) {
             while (reader.hasNextLine()) {
                 String line = reader.nextLine().toLowerCase();
+                // Remove punctuation and non-letter characters
                 line = line.replaceAll("[^a-z0-9\\s]", " ");
                 String[] parts = line.split("\\s+");
                 for (String word : parts) {
@@ -80,32 +109,4 @@ public class ReadFile {
         }
         return words;
     }
-
-    // Analyze word list and return stats
-    public static FileStats analyzeWords(List<String> words) {
-        FileStats stats = new FileStats();
-        stats.totalWords = words.size();
-
-        Map<String, Integer> frequencyMap = new HashMap<>();
-        for (String word : words) {
-            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
-        }
-
-        stats.uniqueWords = frequencyMap.size();
-
-        String topWord = "";
-        int topCount = 0;
-        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
-            if (entry.getValue() > topCount) {
-                topWord = entry.getKey();
-                topCount = entry.getValue();
-            }
-        }
-
-        stats.topWord = topWord;
-        stats.topCount = topCount;
-
-        return stats;
-    }
 }
-
