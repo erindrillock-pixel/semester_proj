376699e45f893acb27f23103a2b9b3e531f784a0
erindrillock-pixel <erin.drillock@student.fairfield.edu>
2025-11-10 11:30:24 -0500

    update

--- src/FileStats.java
+++ src/FileStats.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class FileStats {
-    int totalWords;
-    int uniqueWords;
-    String topWord;
-    int topCount;
-    ArrayList<String> rankedWords;
-    ArrayList<Integer> rankedCounts;
-    double richVocab;
-    int positiveWords;
-    int negativeWords;
-    String lexiConWords;
-}
+        int totalWords;
+        int uniqueWords;
+        String topWord;
+        int topCount;
+        ArrayList<String> rankedWords;
+        ArrayList<Integer> rankedCounts;
+        double richVocab;
+        int positiveWords;
+        int negativeWords;
+        String lexiConWords;
+    }
